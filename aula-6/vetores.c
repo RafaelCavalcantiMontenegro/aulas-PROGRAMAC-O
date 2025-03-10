aaba51e2a5289acfe5637b8642aa33e1399de596
RafaelCavalcantiMontenegro <rafacm2005@gmail.com>
2025-03-10 19:33:00 +0000

    aula-revisão

--- aula-6/vetores.c
+++ aula-6/vetores.c
@@ -9,6 +9,7 @@ int main() {
 
     double notas[5];
     double somatorio = 0.0, media;
+
     for (int i = 0; i < QTD_ALUNOS; i += 1) {
         printf("Digite a mota %d: ", i + 1);
         scanf("%lf", &notas[i]);
@@ -20,10 +21,15 @@ int main() {
     printf("\nMedia da turma: %2.f\n", media);
 
     printf("\nImpressao de todas as notas da turma\n");
-    for (int i = 0; i < QTD_ALUNOS; i =+ 1) {
+    for (int i = 0; i < QTD_ALUNOS; i += 1) {
         printf("Nota %d:%.2f\n", i +1, notas[i]);
-        sacnf("%lf", &notas[i]);
         
     }
-    return 0;
+    printf("\nImpressao de todas as notas acima da media da turma\n");
+    for (int i = 0; i < QTD_ALUNOS; i += 1) {
+      if (notas[i] > media) {
+        printf("Nota %d: %.2f\n", i + 1, notas[i]);
+      }
+    }
+  return 0;
 }
