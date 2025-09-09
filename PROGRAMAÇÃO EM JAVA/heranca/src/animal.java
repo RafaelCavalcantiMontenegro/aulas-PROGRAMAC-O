c5e5d48aff07ee117cde09fe0745d24f33f42f83
RafaelCavalcantiMontenegro <rafacm2005@gmail.com>
2025-09-09 18:58:24 +0000

    aula de java

---
+++ PROGRAMAÇÃO EM JAVA/heranca/src/animal.java
@@ -0,0 +1,13 @@
+public class Animal  {
+    private String nome;
+    private String especie;
+
+    public categoria (String especie, String nome) {
+        this.especie = especie;
+        this.nome = nome;
+    }
+    
+
+
+
+}
---
+++ PROGRAMAÇÃO EM JAVA/heranca/src/aves.java
@@ -0,0 +1,10 @@
+public class Aves extends Animal {
+    private String penas;
+    private String asas;
+    private String bico;
+
+    public Aves (String penas, String asas, String bico) {
+        this.penas = penas;
+        this.asas = as
+        }
+}
